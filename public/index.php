1accfb9c7ed446e0165d44d54c149a91876d6060
MAR7032 <muhammadabdulrehmaneducation@gmail.com>
2026-05-29 16:13:40 +0300

    Version 1 - Create project structure and PHP setup page

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+$pageTitle = "Secure Password Vault";
+$message = "PHP project setup is working.";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $pageTitle; ?></title>
+</head>
+<body>
+    <h1><?php echo $pageTitle; ?></h1>
+    <p><?php echo $message; ?></p>
+</body>
+</html>
